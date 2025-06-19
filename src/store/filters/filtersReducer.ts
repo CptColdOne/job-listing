ae4fae50391705e1044fb1da4b466fe55d411f87
Andrei <panferov-2010@mail.ru>
2025-06-19 16:34:59 +0600

    add persisted

--- src/store/filters/filtersReducer.ts
+++ src/store/filters/filtersReducer.ts
@@ -3,9 +3,9 @@ import { FilterAction } from './types';
 
 const filtersReducer = (state: string[] = [], action: FilterAction) => {
 	if (action.type === ADD_FILTER) {
-		return action.payload && state.includes(action.payload)
-			? state
-			: [...state, action.payload];
+		return action.payload && !state.includes(action.payload)
+			? [...state, action.payload]
+			: state;
 	} else if (action.type === REMOVE_FILTER) {
 		return state.filter((item) => item !== action.payload);
 	} else if (action.type === CLEAR_FILTERS) {
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+	key: 'root',
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store: any = configureStore({
-  reducer: rootReducer
+	reducer: persistedReducer,
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
